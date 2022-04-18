37c5cf67b0fbc2e182fdc533a8395e1d75fd3585
terezamr <terezameloribeiro@gmail.com>
2022-04-18 11:08:29 +0100

    5 day forecast + icon

--- src/DailyTemp.js
+++ src/DailyTemp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
 
@@ -6,33 +6,104 @@ export default function DailyTemp(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.latitud]);
+
   function forecastApp(response) {
     setForecast(response.data.daily);
+    console.log(response.data);
     setLoaded(true);
-    console.log(forecast);
   }
 
   if (loaded) {
     return (
-      <div className="col week">
-        {props.day}
-        <div>
-         
+      <div className="row">
+        {" "}
+        <div className="col week">
+          {props.days[1]}
+          <div>
+            <WeatherIcon
+              size="25"
+              code={forecast[1].weather[0].icon}
+              animation="false"
+            />
+          </div>
+          <p>
+            {Math.round(forecast[1].temp.max)}ºC |{" "}
+            {Math.round(forecast[1].temp.min)}ºC
+          </p>
+          <p></p>
         </div>
-        <p>
-          19ºC
-        </p>
-        <p></p>
+        <div className="col week">
+          {props.days[2]}
+          <div>
+            <WeatherIcon
+              size="25"
+              code={forecast[2].weather[0].icon}
+              animation="false"
+            />
+          </div>
+          <p>
+            {Math.round(forecast[2].temp.max)}ºC |{" "}
+            {Math.round(forecast[2].temp.min)}ºC
+          </p>
+          <p></p>
+        </div>
+        <div className="col week">
+          {props.days[3]}
+          <div>
+            <WeatherIcon
+              size="25"
+              code={forecast[3].weather[0].icon}
+              animation="false"
+            />
+          </div>
+          <p>
+            {Math.round(forecast[3].temp.max)}ºC |{" "}
+            {Math.round(forecast[3].temp.min)}ºC
+          </p>
+          <p></p>
+        </div>
+        <div className="col week">
+          {props.days[4]}
+          <div>
+            <WeatherIcon
+              size="25"
+              code={forecast[4].weather[0].icon}
+              animation="false"
+            />
+          </div>
+          <p>
+            {Math.round(forecast[4].temp.max)}ºC |{" "}
+            {Math.round(forecast[4].temp.min)}ºC
+          </p>
+          <p></p>
+        </div>
+        <div className="col week">
+          {props.days[5]}
+          <div>
+            <WeatherIcon
+              size="25"
+              code={forecast[5].weather[0].icon}
+              animation="false"
+            />
+          </div>
+          <p>
+            {Math.round(forecast[5].temp.max)}ºC |{" "}
+            {Math.round(forecast[5].temp.min)}ºC
+          </p>
+          <p></p>
+        </div>
       </div>
     );
   } else {
     let apiKey = "c1eb44225008106eb8fc583ee3b61e06";
     let latitude = props.latitud;
     let longitude = props.longitud;
- //   let apiUrlFor = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.Latitude}&lon=${props.Longitude}&appid=${apiKey}&units=metric`;
-    let apiUrlFor = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrlFor = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrlFor).then(forecastApp);
-    
-    return "ola";
+
+    return null;
   }
 }
